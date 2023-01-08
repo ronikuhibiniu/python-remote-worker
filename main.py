5ea35067005508d0fa91779788264e35c72e85a2
Roni <ronikuhibiniu@gmail.com>
2023-01-08 22:31:05 +0700

    02

---
+++ main.py
@@ -0,0 +1,23 @@
+#Sekuensial
+print("Hallo")
+print("Saya")
+print("Adalah")
+print("Monster Inc")
+
+#Percabangan
+jlmh_btl_susu = 200
+if jlmh_btl_susu > 0:
+    print("budi memebeli 1 botol susu")
+else:
+    print("Jadi membeli 1 botol susu")
+
+jumlah_telur = 173
+jumlah_susu = 120
+
+def jlmh_total():
+    if jumlah_susu > 0:
+        print("Budi jadi membeli")
+    if jumlah_telur < 200:
+        print("tidak jadi membeli")
+jlmh_total()
+
